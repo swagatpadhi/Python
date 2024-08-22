95efa84dbf5be6389c8305cafaaf4a406037c029
swagatpadhi <swagatpadhi03@gmail.com>
2024-08-22 23:43:33 +0530

    Added chapter_3

---
+++ Chapter_3/string_slicing.py
@@ -0,0 +1,15 @@
+name = "Harry"
+str1 = name[1:3]
+print(str1)
+
+str2 = name[1]
+print(str2)
+
+str3 = name[:4] #is same as name[0:4]
+str4 = name[1:] #is same as name[1:length] i.e name[1:5]
+print(str3)
+print(str4)
+
+str5 = name[-4:-1] #just convert -ve indices into +ve.
+str5 = name[1:4]
+print(str5)
