176347301340e892cf6e435a55fb49cc660abb36
swagatpadhi <swagatpadhi03@gmail.com>
2024-08-25 12:48:23 +0530

    Added Notes to sets.py in Chapter_5.

--- Chapter_5/sets.py
+++ Chapter_5/sets.py
@@ -4,3 +4,11 @@ empty_set = set() #this is an empty set
 print(type(empty_set))
 
 print(s)
+
+# Set Elements Must Be Immutable: While sets 
+# themselves are mutable, the elements within a
+# set must be immutable (e.g., numbers, strings, tuples). This 
+# is because sets rely on the hashability 
+# of their elements,
+# and mutable objects (like lists) cannot
+# be hashed and thus cannot be included in a set.
