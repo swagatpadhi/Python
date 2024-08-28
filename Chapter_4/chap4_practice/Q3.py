30d910dbe389eb31accb5d23108aaed38d198e52
swagatpadhi <swagatpadhi03@gmail.com>
2024-08-28 10:10:29 +0530

    made a correction in Q3.py of chap4_practice.

--- Chapter_4/chap4_practice/Q3.py
+++ Chapter_4/chap4_practice/Q3.py
@@ -1,6 +1,6 @@
 y = (3, 66, "Strings", False, 67.5, 39)
 
-#The following thing is wrong, as Tuples are immutable in Python.
 print(y[0])
+#The following thing is wrong, as Tuples are immutable in Python.
 y[0] = "Hard"
 print(y)
